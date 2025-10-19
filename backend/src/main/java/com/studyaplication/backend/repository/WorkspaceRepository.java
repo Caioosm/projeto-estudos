8a07dff3f1aad6aebee2bc29cc2512c7ac01cde5
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-18 23:50:32 -0300

    refactor (persistence): Adicionar annotation @Repository e organizar imports

--- backend/src/main/java/com/studyaplication/backend/repository/WorkspaceRepository.java
+++ backend/src/main/java/com/studyaplication/backend/repository/WorkspaceRepository.java
@@ -2,8 +2,9 @@ package com.studyaplication.backend.repository;
 
 import com.studyaplication.backend.model.Workspace;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
+@Repository
 public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
 }
