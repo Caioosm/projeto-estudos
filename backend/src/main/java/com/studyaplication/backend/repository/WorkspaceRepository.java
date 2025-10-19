c5da2eacf4caa0b59d60003eddb791be07e795ee
caioosm <matossousacaio.cms@gmail.com>
2025-10-18 23:16:59 -0300

    feat(persistence): Implementar Workspace Repository para persistencia no banco de dados

--- backend/src/main/java/com/studyaplication/backend/repository/WorkspaceRepository.java
+++ backend/src/main/java/com/studyaplication/backend/repository/WorkspaceRepository.java
@@ -1,4 +1,9 @@
 package com.studyaplication.backend.repository;
 
-public interface WorkspaceRepository {
+import com.studyaplication.backend.model.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WorkspaceRepository extends JpaRepository<Workspace, UUID> {
 }
