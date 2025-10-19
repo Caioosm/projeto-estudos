e77dc8b6f287df8dfb866d7fa269819750ebb121
caioosm <matossousacaio.cms@gmail.com>
2025-10-18 22:58:41 -0300

    feat(Model): Entidade Workspace que ira representar o espaco de trabalho do usuario

--- backend/src/main/java/com/studyaplication/backend/model/Workspace.java
+++ backend/src/main/java/com/studyaplication/backend/model/Workspace.java
@@ -1,4 +1,41 @@
 package com.studyaplication.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "workspace",
+        schema = "public"
+)
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Workspace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+
+    /*
+    TODO: Entidade Category
+    private List<Category> category;
+     */
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
 }
