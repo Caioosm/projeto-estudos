f1b8d637376c2b641a32950e172648fd61256e7d
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-18 23:47:55 -0300

    feat (mappe): Criar mapper de Workspace para mapeamento de entidade para DTO e vice versa

---
+++ backend/src/main/java/com/studyaplication/backend/mapper/WorkspaceMapper.java
@@ -0,0 +1,21 @@
+package com.studyaplication.backend.mapper;
+
+import com.studyaplication.backend.dto.request.WorkspaceRequestDTO;
+import com.studyaplication.backend.dto.response.WorkspaceResponseDTO;
+import com.studyaplication.backend.model.Workspace;
+import com.studyaplication.backend.repository.StudentRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring", uses = {StudentMapper.class})
+public abstract class WorkspaceMapper {
+
+    @Autowired
+    StudentRepository repository;
+
+    @Mapping(target = "student", expression = "java ( repository.findById(dto.studentId).orElse(null)")
+    public abstract Workspace toEntity(WorkspaceRequestDTO dto);
+
+    public abstract WorkspaceResponseDTO toDto(Workspace entity);
+}
