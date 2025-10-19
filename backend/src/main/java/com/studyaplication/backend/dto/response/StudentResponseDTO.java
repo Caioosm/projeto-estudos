fc79df66cbf4311ecb97280bdaa87fb58fd5c035
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-18 23:49:30 -0300

    feat (dto): Adicionar role na resposta da request

--- backend/src/main/java/com/studyaplication/backend/dto/response/StudentResponseDTO.java
+++ backend/src/main/java/com/studyaplication/backend/dto/response/StudentResponseDTO.java
@@ -1,7 +1,10 @@
 package com.studyaplication.backend.dto.response;
 
+import com.studyaplication.backend.model.enums.Role;
+
 public record StudentResponseDTO(
         String name,
         String username,
-        String email
+        String email,
+        Role role
 ) {}
